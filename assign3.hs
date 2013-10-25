8542f312daf5fa00068b3cc8cf3d9d657219771c
briguychau <me@brianchau.ca>
2013-10-24 18:00:18 -0700

    Added error checking for initial state of board

--- assign3.hs
+++ assign3.hs
@@ -8,7 +8,17 @@ Assignment 3 CPSC 312
 -}
 
 rush_hour :: [String] -> [[String]]
-rush_hour start = reverse (statesearch [start] [])
+rush_hour start
+	| valid start	= reverse (statesearch [start] [])
+	| otherwise	= error "This is not a Rush Hour board with 6 rows and 6 columns"
+
+valid :: [String] -> Bool
+valid (a:b:c:d:e:f:[]) = (valid_row a) && (valid_row b) && (valid_row c) && (valid_row d) && (valid_row e) && (valid_row f)
+valid _ = False
+
+valid_row :: String -> Bool
+valid_row (_:_:_:_:_:_:[]) = True
+valid_row _ = False
 
 statesearch :: [[String]] -> [[String]] -> [[String]]
 statesearch unexplored path
@@ -19,8 +29,8 @@ statesearch unexplored path
 	where newstates = statesearch (generateNewStates (head unexplored) path) ((head unexplored):path)
 
 isgoal :: [String] -> Bool
-isgoal (a:b:c:d:e:f:[]) = exiting c
-	where exiting (a:b:c:d:e:f:[]) = ((e == 'X') && (f == 'X'))
+isgoal (_:_:x:_:_:_:[]) = exiting x
+	where exiting (_:_:_:_:e:f:[]) = (e == 'X') && (f == 'X')
 
 generateNewStates :: [String] -> [[String]] -> [[String]]
 generateNewStates x explored = removeDupStates (generateAllMoves x) explored
